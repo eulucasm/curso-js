4f31f24132927f8c289536a33e921a2f154b9b60
Lucas <lucaspoletis@gmail.com>
2021-08-15 20:41:25 -0300

    Operadores logicos

---
+++ Seção 2 - Logica/02 - Operadores logicos/index.js
@@ -0,0 +1,11 @@
+/*
+Operadores Lógicos
+&& -> And -> E - Todos os elementos devem ser verdadeiros 
+|| -> Or -> OU - Algum elemento deve ser verdadeiro
+! -> Not -> N - 
+*/
+
+console.log(true && true); //true
+console.log(true && false); //false
+console.log(true || false); //true
+console.log(false || false); //false
