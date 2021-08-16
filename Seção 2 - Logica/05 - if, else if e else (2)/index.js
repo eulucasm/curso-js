1d9b1bebcfba862ddbcf3a9eef8178dc1965986d
Lucas <lucaspoletis@gmail.com>
2021-08-15 22:56:20 -0300

    if, else if e else(2)

---
+++ Seção 2 - Logica/05 - if, else if e else (2)/index.js
@@ -0,0 +1,14 @@
+const numero = 30;
+
+//se (numero >= 0 && numero <= 5 ) ocorrer, faça isso {código}
+//se não faça isso {o código}
+
+if (numero >= 0 && numero <= 5) {
+    console.log('o numero está entre 0 e 5.');
+} else if (numero >= 6 && numero <= 8) {
+    console.log('O numero está entre 6 e 8');
+} else if (numero >= 9 && numero <= 11) {
+    console.log('O numero está entre 9 e 11');
+} else {
+    console.log('O numero não esta entra 0 e 11 ');
+}
