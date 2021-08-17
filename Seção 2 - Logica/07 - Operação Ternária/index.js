eae5969852281dcbc41e8cd7a01e724a906a802e
Lucas <lucaspoletis@gmail.com>
2021-08-17 17:10:11 -0300

    Operação ternária

---
+++ Seção 2 - Logica/07 - Operação Ternária/index.js
@@ -0,0 +1,12 @@
+//(condição) ?'Valor para verdadeiro' : 'Valor para falso'
+
+const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario Vip' : 'Usuario Normal';
+console.log(nivelUsuario);
+
+
+// if (pontuacaoUsuario >= 1000) {
+//     console.log('VIP');
+// } else {
+//     console.log('Normal');
+// }
