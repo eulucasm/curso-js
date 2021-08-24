2632657590983c97efe608e4fd631d6324a19b81
Lucas <lucaspoletis@gmail.com>
2021-08-24 18:30:42 -0300

    Explicação de DOM

---
+++ Seção 2 - Logica/16 - DOM e a árvore do DOM/index.js
@@ -0,0 +1,19 @@
+/*
+Modelo de Documento por Objetos - Document Object Model
+aula sobre dom
+o conceito de árvore se da por conta dos filhos dos filhos em formato de árvore
+afunilando até o pai principal.
+na estrutura do html temos a seguinte estrutura básica:
+
+window.document
+#document
+<html>
+<head>    
+</head>
+<body>
+</body>
+</html> 
+com isso, sabemos que a body é filha da html, assim como a head
+e a html é filha da document.
+e consequentemente document é filha de window
+*/
