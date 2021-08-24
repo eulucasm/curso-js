895a45672666a0d39ec299e553fbd28e51dba1e4
Lucas <lucaspoletis@gmail.com>
2021-08-24 16:58:21 -0300

    For - classico - estrutura de repetição

---
+++ Seção 2 - Logica/14 - For - Classico - Estrutura de repetição/index.js
@@ -0,0 +1,33 @@
+// console.log('linha 0');
+// console.log('linha 1');
+// console.log('linha 2');
+// console.log('linha 3');
+// console.log('linha 4');
+// console.log('linha 5');
+
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Melancia', 'Jaca', 'Manga', 'banana'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`indice ${i}`, frutas[i]);
+}
+
+// for (let i = 0; i <= 10; i++) {
+//     const par = i % 2 === 0 ? 'par' : 'ímpar';
+//     console.log(i, par);
+// }
+
+// subtraindo de 10 em 10
+// for (let i = 500; i >= 400; i -= 10) {
+//     console.log(`linha ${i}`);
+// }
+
+//indo de 10 em 10
+// for (let i = 400; i <= 500; i += 10) {
+//     console.log(`linha ${i}`);
+// }
+
+//i - index
+// for (let i = 0; i <= 5; i++) {
+//     console.log(`linha ${i}`);
+
+// }
