13dc72475706dcc5e470b7a58fa257dd94e28ffc
Lucas <lucaspoletis@gmail.com>
2021-09-01 18:53:16 -0300

    IIFE

--- Seção 3 - JavaScript Funções (Avançado)/06 - Função de callback/index.js
+++ Seção 3 - JavaScript Funções (Avançado)/06 - Função de callback/index.js
@@ -1,3 +1,4 @@
+//funções de callback
 function rand(min = 1000, max = 3000) {
     const num = Math.random() * (max - min) + min;
     return Math.floor(num);
---
+++ Seção 3 - JavaScript Funções (Avançado)/07 - Funções imediatas (IIFE)/index.js
@@ -0,0 +1,7 @@
+//IIFE -> immediately invoked function expression
+
+(function () {
+    console.log(132112312)
+})();
+//nesse caso, chamamos nossa função imediatamente após ser declarada
+//ela é anonima usada para ser executada diretamente.
