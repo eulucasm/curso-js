4275d481698bb29a4903cf1936003478923ed1c3
Lucas <lucaspoletis@gmail.com>
2021-09-01 22:46:00 -0300

    IIFE

--- Seção 3 - JavaScript Funções (Avançado)/07 - Funções imediatas (IIFE)/index.js
+++ Seção 3 - JavaScript Funções (Avançado)/07 - Funções imediatas (IIFE)/index.js
@@ -4,4 +4,19 @@
     console.log(132112312)
 })();
 //nesse caso, chamamos nossa função imediatamente após ser declarada
-//ela é anonima usada para ser executada diretamente.
+//ela é anónima usada para ser executada diretamente.
+//exemplo
+
+(function (idade, peso, altura) {
+    const sobrenome = 'Marques';
+
+    function criaNome(nome) {
+        return nome + ' ' + sobrenome;
+    }
+
+    function falaNome() {
+        console.log(criaNome('Lucas'));
+    }
+    falaNome();
+    console.log(idade, peso, altura);
+})(30, 80, 1.80);
