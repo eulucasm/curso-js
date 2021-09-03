2571a90d4ef1ae7a56e87d4381a2abbd15a89bfd
Lucas <lucaspoletis@gmail.com>
2021-09-03 12:06:12 -0300

    Método Splice

---
+++ Seção 4 - JavaScript Arrays (Avançado)/02 - Método Splice/index.js
@@ -0,0 +1,19 @@
+//              -5        -4        -3        -2         -1
+//               0         1         2         3          4     
+const nomes = ['Lucas', 'Maria', 'Angela', 'Matheus', 'Aurelio'];
+
+//nomes.splice(indice a começar, quantos vou deletar, quantos vou add )
+
+//pop
+//const removidos = nomes.splice(-1, 1)
+
+//shift
+//const removidos = nomes.splice(0, 1)
+
+//push
+//nomes.splice(nomes.length, 0, 'Eduardo');
+
+//unshift
+//nomes.splice(0, 0, 'Barbara');
+
+console.log(nomes);
