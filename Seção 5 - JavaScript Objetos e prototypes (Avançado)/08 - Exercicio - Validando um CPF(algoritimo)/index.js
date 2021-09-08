d5908c83d400be8f3bf9f9fe673ae813ef9bbc5d
Lucas <lucaspoletis@gmail.com>
2021-09-07 22:34:50 -0300

    Validando CPF

--- Seção 5 - JavaScript Objetos e prototypes (Avançado)/08 - Exercicio - Validando um CPF(algoritimo)/index.js
+++ Seção 5 - JavaScript Objetos e prototypes (Avançado)/08 - Exercicio - Validando um CPF(algoritimo)/index.js
@@ -4,10 +4,12 @@
 10 9  8  7  6  5  4  3  2
 70 0  40 28 48 20 16 15 0
 
+11 - (237 % 11) = 5 (primeiro digito)
+
 7x 0x 5x 4x 8x 4x 4x 5x 0x 5x
 11 10 9  8  7  6  5  4  3 2 
 77 0  45 32 56 24 20 20 0 10
 
-11 - (237 % 11) = 5 (primeiro digito)
-11 - (284 % 11) = 5 (primeiro digito)
+11 - (284 % 11) = 2 (primeiro digito)
+
 */
