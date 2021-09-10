3bc6704d558989d6fe4bd49892bdbddc545e330d
Lucas <lucaspoletis@gmail.com>
2021-09-10 14:13:19 -0300

    async, await

---
+++ Seção 7 - JavaScript assíncrono - Promisses, Ajax, Fetch e async-await/03 - Async - Await/index.js
@@ -0,0 +1,63 @@
+function rand(min = 0, max = 3) {
+
+    min *= 1000;
+    max *= 1000;
+
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof msg !== 'string') {
+                reject('CAI NO ERRO');
+                return;
+            }
+
+            resolve(msg.toUpperCase() + ' - Passei na promise');
+            return;
+        }, tempo);
+    });
+}
+
+//metodo Async - Await
+async function executa() {
+
+    try {
+
+        const fase1 = await esperaAi('Fase 1', rand());
+        console.log(fase1);
+
+        const fase2 = await esperaAi('Fase 2', rand());
+        console.log(fase2);
+
+        const fase3 = await esperaAi('Fase 3', rand());
+        console.log(fase3);
+
+        console.log('Terminamos na fase: ', fase3);
+    } catch (e) {
+        console.log(e)
+    }
+}
+executa();
+
+
+
+//metodo normal encadeado.
+// esperaAi('Fase 1', rand())
+//     .then(valor => {
+//         console.log(valor);
+//         return esperaAi('fase 2', rand());
+//     })
+//     .then(fase => {
+//         console.log(fase);
+//         return esperaAi('fase 3', rand());
+//     })
+//     .then(fase => {
+//         console.log(fase);
+//         return fase;
+//     })
+//     .then(fase => {
+//         console.log('Terminamos na fase: ', fase)
+//     })
+//     .catch(err => console.log(err));
