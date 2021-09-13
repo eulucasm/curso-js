fb64f032889f2c56737c2baf91170f777247e195
Lucas <lucaspoletis@gmail.com>
2021-09-13 14:44:39 -0300

    preparo de um novo modelo pra uso

--- 01 - Modelos/03 - modelo webpack + style/public/assets/js/bundle.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-alert('Ola mundo');
-/******/ })()
-;
-//# sourceMappingURL=bundle.js.map
