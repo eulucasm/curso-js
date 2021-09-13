fe4f1217753118b410fea7c6c3ff87d28109fac0
Lucas <lucaspoletis@gmail.com>
2021-09-13 18:11:00 -0300

    Aula 1 modulos

---
+++ Seção 9 - JavaScript - Node, Npm, Express e mongoDB(NODEJS)/01-modulo/app.js
@@ -0,0 +1,21 @@
+const {
+    Pessoa
+} = require('./mod1');
+
+const p1 = new Pessoa('Lucas');
+console.log(p1);
+
+
+
+// const nome = 'lucas';
+// const sobrenome = 'marques';
+
+// const falaNome = () => {
+//     console.log(nome, sobrenome);
+// }
+
+// exports.nome = nome;
+// exports.sobrenome = sobrenome;
+// exports.falaNome = falaNome;
+
+// this.qualquerCoisa = 'O que eu quiser exportar';
---
+++ Seção 9 - JavaScript - Node, Npm, Express e mongoDB(NODEJS)/01-modulo/mod1.js
@@ -0,0 +1,7 @@
+//const mod1 = require('./mod1');
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome;
+    }
+}
+exports.Pessoa = Pessoa;
