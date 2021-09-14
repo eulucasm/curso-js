f62c49003deafaed9c9ae618ad2b1de0a5f41190
Lucas <lucaspoletis@gmail.com>
2021-09-14 13:15:22 -0300

    Aula 2 modulos

---
+++ Seção 9 - JavaScript - Node, Npm, Express e mongoDB(NODEJS)/02-modulo/X/Y/app.js
@@ -0,0 +1,9 @@
+const multiplicacao = require('../../A/B/C/D/mod') //Importando um arquivo em subpastas
+
+
+console.log(multiplicacao(2, 2));
+
+
+
+console.log(__filename); //caminho absoluto para o arquivo
+console.log(__dirname); //caminho absoluto para a pasta
