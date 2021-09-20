0bd87096d9ac4e05c3a8f84f0189cf5e504053db
Lucas <lucaspoletis@gmail.com>
2021-09-20 16:50:25 -0300

    Finalizando aula de conexão com mongo e criando um item de teste

--- Seção 9 - JavaScript - Node, Npm, Express e mongoDB(NODEJS)/14-CriandoServerMongo/src/controllers/homeController.js
+++ Seção 9 - JavaScript - Node, Npm, Express e mongoDB(NODEJS)/14-CriandoServerMongo/src/controllers/homeController.js
@@ -1,3 +1,12 @@
+const HomeModel = require('../models/HomeModel');
+
+HomeModel.create({
+        titulo: 'Um Titulo Qualquer de teste',
+        descricao: 'Apenas uma descrição simplório'
+    })
+    .then(dados => console.log(dados))
+    .catch(e => console.log(e));
+
 exports.paginaInicial = (req, res) => {
     res.render('index');
 };
--- Seção 9 - JavaScript - Node, Npm, Express e mongoDB(NODEJS)/14-CriandoServerMongo/src/models/homeModel.js
+++ Seção 9 - JavaScript - Node, Npm, Express e mongoDB(NODEJS)/14-CriandoServerMongo/src/models/homeModel.js
@@ -1,1 +1,14 @@
 const mongoose = require('mongoose');
+
+const HomeSchema = new mongoose.Schema({
+    //modelando o bando de dados
+    titulo: {
+        type: String,
+        require: true
+    },
+    descricao: String
+});
+
+const HomeModel = mongoose.model('Home', HomeSchema);
+
+module.exports = HomeModel;
