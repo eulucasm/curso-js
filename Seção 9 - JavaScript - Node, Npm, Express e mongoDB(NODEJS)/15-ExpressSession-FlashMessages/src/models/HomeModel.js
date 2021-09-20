acb0e682177e6f802fb65b066c4e3caee84f54bb
Lucas <lucaspoletis@gmail.com>
2021-09-20 18:09:13 -0300

    configurando session e flash

---
+++ Seção 9 - JavaScript - Node, Npm, Express e mongoDB(NODEJS)/15-ExpressSession-FlashMessages/src/models/HomeModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const HomeSchema = new mongoose.Schema({
+    //modelando o bando de dados
+    titulo: {
+        type: String,
+        require: true
+    },
+    descricao: String
+});
+
+const HomeModel = mongoose.model('Home', HomeSchema);
+
+class Home {
+
+}
+
+module.exports = Home;
