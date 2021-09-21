79eed7bb7067e0225d0c1eda097be29b15b9cebb
Lucas <lucaspoletis@gmail.com>
2021-09-21 17:08:53 -0300

    Add criação de usuario, e flash messagens de verificação

--- Seção10-ProjetoAgenda/src/controllers/loginController.js
+++ Seção10-ProjetoAgenda/src/controllers/loginController.js
@@ -1,3 +1,29 @@
+const Login = require('../models/LoginModel');
+
 exports.index = (req, res) => {
     res.render('login');
 }
+
+exports.register = async function (req, res) {
+    try {
+        const login = new Login(req.body); //instância da Classe login
+        await login.register();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+                return res.redirect('/login/index');
+            });
+            return;
+        }
+        req.flash('success', 'Usuário criado com sucesso');
+        req.session.save(function () {
+            return res.redirect('/login/index');
+        });
+
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+
+};
--- Seção10-ProjetoAgenda/src/middlewares/middleware.js
+++ Seção10-ProjetoAgenda/src/middlewares/middleware.js
@@ -1,5 +1,6 @@
 exports.middlewareGlobal = (req, res, next) => {
-    res.locals.umaVariavelLocal = 'Este é o valor da Variavel.';
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
     next();
 }
 
