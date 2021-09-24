8ccb2aedad73a2c3d5ab891ef3d06c399c53a5a4
Lucas <lucaspoletis@gmail.com>
2021-09-24 11:51:13 -0300

    tratando usuarios logados e direcionado corretamente as rotas

--- Seção10-ProjetoAgenda/src/models/LoginModel.js
+++ Seção10-ProjetoAgenda/src/models/LoginModel.js
@@ -24,6 +24,26 @@ class Login {
         this.user = null;
     }
 
+    async login() {
+
+        this.valida();
+        if (this.errors.length > 0) return;
+        this.user = await LoginModel.findOne({
+            email: this.body.email
+        });
+
+        if (!this.user) {
+            this.errors.push('Usuário não existe.');
+            return;
+        }
+
+        if (!bcryptjs.compareSync(this.body.password, this.user.password)) {
+            this.errors.push('Senha Invalida');
+            this.user = null;
+            return;
+        }
+    }
+
     async register() {
         this.valida();
         if (this.errors.length > 0) return;
@@ -34,19 +54,16 @@ class Login {
         const salt = bcryptjs.genSaltSync();
         this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
-        try {
-            this.user = await LoginModel.create(this.body);
-        } catch (e) {
-            console.log(e)
-        }
+
+        this.user = await LoginModel.create(this.body);
+
     }
 
     async userExists() {
-        const user = await LoginModel.findOne({
+        this.user = await LoginModel.findOne({
             email: this.body.email
         });
-
-        if (user) this.errors.push('Usuário Já existe');
+        if (this.user) this.errors.push('Usuário Já existe');
 
     }
 
