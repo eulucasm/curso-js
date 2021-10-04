3cae3f4942c8ec8dc1cfe2b2ad203cd114c34705
eulucasm <lucaspoletis@gmail.com>
2021-10-04 17:47:13 -0300

    Fechando a rota arquivos com token

--- api_rest/src/routes/fotoRoutes.js
+++ api_rest/src/routes/fotoRoutes.js
@@ -1,11 +1,12 @@
 import {
   Router
 } from 'express';
+import loginRequired from '../middlewares/loginRequired';
 
 import fotoController from '../controllers/FotoController';
 
 const router = new Router();
 
-router.post('/', fotoController.store);
+router.post('/', loginRequired, fotoController.store);
 
 export default router;
