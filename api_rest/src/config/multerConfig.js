c9ff5e8299facc9aac43c36ca04bf38b8b960e32
eulucasm <lucaspoletis@gmail.com>
2021-10-04 16:52:44 -0300

    Upload de arquivos com Multer

---
+++ api_rest/src/config/multerConfig.js
@@ -0,0 +1,25 @@
+import multer from 'multer';
+import {
+  extname,
+  resolve
+} from 'path';
+
+const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
+
+export default {
+  fileFilter: (req, file, callback) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return callback(new multer.MulterError('Externções permitidas: PNG ou JPEG'));
+    }
+
+    return callback(null, true);
+  },
+  storage: multer.diskStorage({
+    destination: (req, file, callback) => {
+      callback(null, resolve(__dirname, '..', '..', 'uploads'));
+    },
+    filename: (req, file, callback) => {
+      callback(null, `${Date.now()}_${aleatorio()}${extname(file.originalname)}`);
+    }
+  }),
+};
